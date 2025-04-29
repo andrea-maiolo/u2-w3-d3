04d8c1771df0e8b8d2ed5258e354e24d738dd125
Andrea Maiolo <maiolo.m.2@gmail.com>
2025-04-29 16:32:47 +0200

    added cart, function to add and remove books form display

--- main.js
+++ main.js
@@ -1,19 +1,35 @@
 const container = document.getElementsByClassName("container")[0];
+const row = document.getElementsByClassName("row")[0];
+const cartContainer = document.getElementsByClassName("cartContainer")[0];
+const myList = document.getElementById("myList");
+let cart = [];
 
 const populateLibrary = function (array) {
   console.log(array);
   array.forEach((element) => {
-    let card = document.createElement("div");
-    card.innerHTML = `<div class="card">
+    let col = document.createElement("div");
+    col.classList.add("col-md-3");
+
+    col.innerHTML = `<div class="card">
     <img src="${element.img}" class="card-img-top img-fluid" alt="book cover">
     <div class="card-body">
       <h5 class="card-title">${element.title}</h5>
       <p class="card-text">${element.price}</p>
-      <a href="#" class="btn btn-primary">Scarta</a>
+      <button class="btn btn-primary">Scarta</button>
+      <button class="btn btn-success">Compra ora</button>
     </div>
   </div>`;
-    container.appendChild(card);
+
+    row.appendChild(col);
   });
+  checkstorage();
+  addEvents();
+};
+
+const checkstorage = function () {
+  if (localStorage.length != 0) {
+    cart = JSON.parse(localStorage.getItem("myBooks"));
+  }
 };
 
 const fetchBooks = function () {
@@ -27,6 +43,47 @@ const fetchBooks = function () {
       populateLibrary(data);
     })
     .catch((error) => console.error(error));
+};
+
+const updateCart = function () {
+  myList.innerHTML = "";
+  cart.forEach((book) => {
+    let listItem = document.createElement("li");
+    listItem.innerText = book;
+    let removeCartBtn = document.createElement("button");
+    removeCartBtn.innerText = "Rimuovi dal carello";
+    removeCartBtn.addEventListener("click", removeFromCart);
+    listItem.appendChild(removeCartBtn);
+    myList.appendChild(listItem);
+  });
+};
+
+const removeFromCart = function (event) {
+  let bookToRemoveFromCart = this.parentElement;
+  bookToRemoveFromCart.remove();
+};
+
+const saveCart = function () {
+  localStorage.setItem("myBooks", JSON.stringify(cart));
+};
+
+const discard = function () {
+  const cardToRemove = this.parentElement.parentElement;
+  cardToRemove.remove();
+};
+
+const addToCart = function (event) {
+  let bookToAdd = this.parentElement.querySelector(".card-title").innerText;
+  cart.push(bookToAdd);
+  updateCart();
+  saveCart();
+};
+
+const addEvents = function () {
+  const btnsRemove = document.querySelectorAll(".btn-primary");
+  btnsRemove.forEach((btn) => btn.addEventListener("click", discard));
+  const btnsBuy = document.querySelectorAll(".btn-success");
+  btnsBuy.forEach((btn) => btn.addEventListener("click", addToCart));
 };
 
 window.addEventListener("load", fetchBooks);

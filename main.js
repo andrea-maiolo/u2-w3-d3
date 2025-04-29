59ccda69369c71091265da52d35936e709ce128e
Andrea Maiolo <maiolo.m.2@gmail.com>
2025-04-29 16:54:53 +0200

    added functions to remove book form localstorage and cart

--- main.js
+++ main.js
@@ -6,15 +6,21 @@ let cart = [];
 
 const populateLibrary = function (array) {
   console.log(array);
+
+  if (localStorage.length != 0) {
+    cart = JSON.parse(localStorage.getItem("myBooks"));
+  }
+  updateCart();
+
   array.forEach((element) => {
     let col = document.createElement("div");
-    col.classList.add("col-md-3");
+    col.classList.add("col-md-4");
 
-    col.innerHTML = `<div class="card">
-    <img src="${element.img}" class="card-img-top img-fluid" alt="book cover">
+    col.innerHTML = `<div class="card mb-2" >
+    <img src="${element.img}" class="card-img-top img-fluid" style="height:15rem" alt="book cover">
     <div class="card-body">
       <h5 class="card-title">${element.title}</h5>
-      <p class="card-text">${element.price}</p>
+      <p class="card-text">${element.price}$</p>
       <button class="btn btn-primary">Scarta</button>
       <button class="btn btn-success">Compra ora</button>
     </div>
@@ -22,14 +28,7 @@ const populateLibrary = function (array) {
 
     row.appendChild(col);
   });
-  checkstorage();
   addEvents();
-};
-
-const checkstorage = function () {
-  if (localStorage.length != 0) {
-    cart = JSON.parse(localStorage.getItem("myBooks"));
-  }
 };
 
 const fetchBooks = function () {
@@ -49,18 +48,26 @@ const updateCart = function () {
   myList.innerHTML = "";
   cart.forEach((book) => {
     let listItem = document.createElement("li");
+    listItem.classList.add("text-white");
     listItem.innerText = book;
     let removeCartBtn = document.createElement("button");
-    removeCartBtn.innerText = "Rimuovi dal carello";
+    removeCartBtn.innerHTML = `<i class="bi bi-trash"></i>`;
     removeCartBtn.addEventListener("click", removeFromCart);
+    removeCartBtn.classList.add("ms-2");
     listItem.appendChild(removeCartBtn);
     myList.appendChild(listItem);
   });
 };
 
-const removeFromCart = function (event) {
+const removeFromCart = function () {
   let bookToRemoveFromCart = this.parentElement;
+  //this remove the dom
   bookToRemoveFromCart.remove();
+  //this remove from array
+  let indexToRemove = cart.indexOf(bookToRemoveFromCart.innerText);
+  cart.splice(indexToRemove, 1);
+  //this updates localstorage
+  saveCart();
 };
 
 const saveCart = function () {
